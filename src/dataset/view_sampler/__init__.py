10b073286a94b862e669bf1d63ca7c6d0e99cdae
Quinton Petty <qpetty@apple.com>
2025-11-07 17:21:05 -0800

    more print

--- src/dataset/view_sampler/__init__.py
+++ src/dataset/view_sampler/__init__.py
@@ -34,7 +34,7 @@ def get_view_sampler(
     cameras_are_circular: bool,
     step_tracker: StepTracker | None,
 ) -> ViewSampler[Any]:
-    print("{cfg.name}")
+    print(f'cfg.name: {cfg.name}')
     return VIEW_SAMPLERS[cfg.name](
         cfg,
         stage,
